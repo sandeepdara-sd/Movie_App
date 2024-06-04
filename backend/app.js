270ff6097c7fa970ff09005dd0b7653679682e0b
sandeepdara-sd <148864994+sandeepdara-sd@users.noreply.github.com>
2024-06-04 16:50:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
-import blogRouter from "./routes/movie-routes.js";
 import cors from 'cors'
 import router from "./routes/user-routes.js";
+import dotenv from 'dotenv';
+import movieRouter from "./routes/movie-routes.js";
+
+
 
 const app = express();
 const PORT = 5000;
 app.use(cors())
 app.use(express.json())
 app.use("/api/user/",router);
-app.use("/api/movies/", blogRouter);
+app.use("/api/movies/", movieRouter);
 
-mongoose.connect("mongodb+srv://sandeepdara44:kmss1234@cluster0.xa1hnza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+dotenv.config();
+mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.xa1hnza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(() => {
     app.listen(PORT, () => {
         console.log("Server running on 5000");
